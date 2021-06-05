2156b1c9a9e0d1ff3c8ca6c526d8c6055be9b3d5
Mike Brind <mikebrind@stepcomms.com>
2021-06-05 07:59:34 +0100

    Added basic middleware examples

---
+++ Chapter02/MiddlewareExample01/Startup.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddlewareExample01
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.Use(PassThroughMiddleware);
+
+            app.Run(TerminalMiddleware);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+        private async Task PassThroughMiddleware(HttpContext context, Func<Task> next)
+        {
+            if (context.Request.Query.ContainsKey("stop"))
+            {
+                await context.Response.WriteAsync("Stop the world");
+            }
+            else
+            {
+                await next();
+            }
+        }
+
+        private async Task TerminalMiddleware(HttpContext context)
+        {
+            await context.Response.WriteAsync("That's all, folks!");
+        }
+
+    }
+}
