37af2a28a4e32c3141011f80b945d33c3b873029
Mike Brind <mikebrind@stepcomms.com>
2021-06-05 08:10:22 +0100

    AddedIMiddleware example

---
+++ Chapter02/IMiddlewareExample/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace IMiddlewareExample
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseIpAddressMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<IpAddressMiddleware>();
+        }
+    }
+}
