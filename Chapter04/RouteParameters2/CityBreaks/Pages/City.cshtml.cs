7b2ff9a7de8a4153f17a4440943802c900982e99
Mike Brind <mikebrind@stepcomms.com>
2021-07-03 08:00:31 +0100

    Added RouteParameters2

---
+++ Chapter04/RouteParameters2/CityBreaks/Pages/City.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages
+{
+    public class CityModel : PageModel
+    {
+        public string CityName { get; set; }
+        public void OnGet(string cityName)
+        {
+            CityName = cityName;
+        }
+
+    }
+}
