de65c90d1574820ec6695483fb50c16b19f6540f
Mike Brind <mikebrind@stepcomms.com>
2021-07-20 06:48:30 +0100

    Starting chapter 5

---
+++ Chapter05/BindingComplexObjects/CityBreaks/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityBreaks.Models
+{
+    public class Country
+    {
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+    }
+
+}
---
+++ Chapter05/RequestForm/CityBreaks/Pages/CityManager/Create.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Primitives;
+
+namespace CityBreaks.Pages.CityManager
+{
+    public class CreateModel : PageModel
+    {
+        public string Message { get; set; } 
+        public void OnPost()
+        {
+            if (!StringValues.IsNullOrEmpty(Request.Form["cityName"]))
+            {
+                Message = $"You submitted {Request.Form["cityName"]}";
+            }
+        }
+
+    }
+}
