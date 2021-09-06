43eb300aca255e79454db41613c38541702b4b07
Mike Brind <mikebrind@stepcomms.com>
2021-09-06 07:27:31 +0100

    Migration

---
+++ Chapter08/CRUDScaffolding/CityBreaks/Services/CityService.cs
@@ -0,0 +1,25 @@
+﻿using CityBreaks.Data;
+using CityBreaks.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityBreaks.Services;
+public class CityService : ICityService
+{
+    private readonly CityBreaksContext _context;
+    public CityService(CityBreaksContext context) => _context = context;
+    public async Task<List<City>> GetCities()
+    {
+        var cities = _context.Cities
+                 .Include(c => c.Country)
+                 .Include(c => c.Properties);
+        return await cities.ToListAsync();
+    }
+
+    public async Task<City> GetCityByName(string name)
+    {
+        return await _context.Cities
+            .Include(c => c.Country)
+            .Include(c => c.Properties)
+            .SingleOrDefaultAsync(c => EF.Functions.Collate(c.Name, "NOCASE") == name);
+    }
+}
---
+++ Chapter08/CRUDScaffolding/CityBreaks/Services/ICityService.cs
@@ -0,0 +1,7 @@
+﻿using CityBreaks.Models;
+namespace CityBreaks.Services;
+public interface ICityService
+{
+    Task<List<City>> GetCities();
+    Task<City> GetCityByName(string name);
+}
