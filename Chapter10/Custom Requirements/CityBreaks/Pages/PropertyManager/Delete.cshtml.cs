4dc9ba3d6244fad7a077025825efaf933ff6ea0e
Mike Brind <mikebrind@stepcomms.com>
2021-10-25 07:27:37 +0100

    Custom Requirements

---
+++ Chapter10/Custom Requirements/CityBreaks/Pages/PropertyManager/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CityBreaks.Data;
+using CityBreaks.Models;
+
+namespace CityBreaks.Pages.PropertyManager
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly CityBreaks.Data.CityBreaksContext _context;
+
+        public DeleteModel(CityBreaks.Data.CityBreaksContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Property Property { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Property = await _context.Properties
+                .Include(p => p.City).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Property == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Property = await _context.Properties.FindAsync(id);
+
+            if (Property != null)
+            {
+                _context.Properties.Remove(Property);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Chapter10/Custom Requirements/CityBreaks/Program.cs
@@ -0,0 +1,91 @@
+using CityBreaks.CustomRouteContraints;
+using CityBreaks.Data;
+using CityBreaks.Models;
+using CityBreaks.PageRouteModelConventions;
+using CityBreaks.ParameterTransformers;
+using CityBreaks.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add builder.Services to the container.
+builder.Services.AddRazorPages(options => {
+    //options.Conventions.AddPageRoute("/Index", "FindMe");
+    options.Conventions.AuthorizeFolder("/CityManager");
+    options.Conventions.AuthorizeFolder("/CountryManager");
+    options.Conventions.AuthorizeFolder("/PropertyManager");
+    options.Conventions.AuthorizeFolder("/RolesManager", "ViewRolesPolicy");
+    options.Conventions.Add(new CultureTemplatePageRouteModelConvention());
+    options.Conventions.Add(new PageRouteTransformerConvention(new KebabPageRouteParameterTransformer()));
+});
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+    options.ConstraintMap.Add("city", typeof(CityRouteConstraint));
+    options.ConstraintMap.Add("slug", typeof(SlugParameterTransformer));
+});
+
+builder.Services.AddDbContext<CityBreaksContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("CityBreaksContext"));
+});
+builder.Services.AddDefaultIdentity<CityBreaksUser>(options => {
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<CityBreaksContext>();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = "/identity/account/access-denied";
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ViewRolesPolicy", policyBuilder => 
+        policyBuilder.RequireAssertion(context =>
+        {
+            var joiningDateClaim = context.User.FindFirst(c => c.Type == "Joining Date")?.Value;
+            var joiningDate = Convert.ToDateTime(joiningDateClaim);
+            return context.User.HasClaim("Permission", "View Roles") && 
+                joiningDate > DateTime.MinValue &&
+                joiningDate < DateTime.Now.AddMonths(-6);
+    }));
+});
+
+builder.Services.AddScoped<ICityService, CityService>(); 
+builder.Services.AddTransient<LifetimeDemoService>();
+builder.Services.AddSingleton<SingletonService>();
+builder.Services.AddScoped<IPriceService, FrPriceService>();
+builder.Services.AddScoped<IPriceService, GbPriceService>();
+builder.Services.AddScoped<IPriceService, UsPriceService>();
+builder.Services.AddScoped<IPriceService, DefaultPriceService>();
+builder.Services.AddScoped<IPropertyService, PropertyService>();
+builder.Services.AddTransient<IEmailSender, EmailService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
