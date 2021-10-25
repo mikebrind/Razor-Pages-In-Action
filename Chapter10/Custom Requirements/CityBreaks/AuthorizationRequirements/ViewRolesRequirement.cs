836c585ef19008f227ad1166481f15ab10ba9199
Mike Brind <mikebrind@stepcomms.com>
2021-10-25 20:41:52 +0100

    View Roles Requirement

--- Chapter10/Custom Requirements/CityBreaks/AuthorizationRequirements/ViewRolesRequirement.cs
+++ Chapter10/Custom Requirements/CityBreaks/AuthorizationRequirements/ViewRolesRequirement.cs
@@ -12,12 +12,12 @@ namespace CityBreaks.AuthorizationRequirements
 
         public Task HandleAsync(AuthorizationHandlerContext context)
         {
-            var joiningDateClaim = context.User.FindFirst(c => c.Type == "Joining Date")?.Value;
-            var joiningDate = Convert.ToDateTime(joiningDateClaim);
             if (context.User.IsInRole("Admin"))
             {
                 context.Succeed(this);
             }
+            var joiningDateClaim = context.User.FindFirst(c => c.Type == "Joining Date")?.Value;
+            var joiningDate = Convert.ToDateTime(joiningDateClaim);
             if(context.User.HasClaim("Permission", "View Roles") &&
                 joiningDate > DateTime.MinValue &&
                 joiningDate < DateTime.Now.AddMonths(Months))
