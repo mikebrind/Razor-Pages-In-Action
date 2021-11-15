d6891ba73f0f930d275d65a91f80974e3d1bf433
Mike Brind <mikebrind@live.co.uk>
2021-11-15 13:32:11 +0000

    Minimal API

---
+++ Chapter11/MinimalAPIJS/CityBreaks/AuthorizationHandlers/PropertyAuthorizationHandler.cs
@@ -0,0 +1,31 @@
+﻿using CityBreaks.AuthorizationRequirements;
+using CityBreaks.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using System.Security.Claims;
+
+namespace CityBreaks.AuthorizationHandlers
+{
+    public class PropertyAuthorizationHandler : AuthorizationHandler<OperationAuthorizationRequirement, Property>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, 
+            OperationAuthorizationRequirement requirement, Property resource)
+        {
+            if(requirement.Name == PropertyOperations.Edit.Name)
+            {
+                if (resource.CreatorId == context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value)
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            if(requirement.Name == PropertyOperations.Delete.Name)
+            {
+                if (context.User.IsInRole("Admin"))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Chapter11/MinimalAPIJS/CityBreaks/Services/LifetimeDemoService.cs
@@ -0,0 +1,11 @@
+﻿namespace CityBreaks.Services
+{
+    public class LifetimeDemoService
+    {
+        public LifetimeDemoService()
+        {
+            Value = Guid.NewGuid();
+        }
+        public Guid Value { get; }
+    }
+}
