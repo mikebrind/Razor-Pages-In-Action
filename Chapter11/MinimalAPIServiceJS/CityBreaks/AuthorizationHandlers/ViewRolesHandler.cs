a0fc957026092b78c8b7a243b54c0e3ef170941d
Mike Brind <mikebrind@live.co.uk>
2021-11-22 06:40:56 +0000

    Updates

---
+++ Chapter11/MinimalAPIServiceJS/CityBreaks/AuthorizationHandlers/ViewRolesHandler.cs
@@ -0,0 +1,32 @@
+﻿using CityBreaks.AuthorizationRequirements;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CityBreaks.AuthorizationHandlers
+{
+    public class ViewRolesHandler : IAuthorizationHandler
+    {
+        public Task HandleAsync(AuthorizationHandlerContext context)
+        {
+            foreach (var requirement in context.PendingRequirements.ToList())
+            {
+                if (requirement is ViewRolesRequirement req)
+                {
+                    var joiningDateClaim = context.User.FindFirst(c => c.Type == "Joining Date")?.Value;
+                    if (joiningDateClaim == null)
+                    {
+                        return Task.CompletedTask;
+                    }
+                    var joiningDate = Convert.ToDateTime(joiningDateClaim);
+
+                    if (context.User.HasClaim("Permission", "View Roles") &&
+                          joiningDate < DateTime.Now.AddMonths(req.Months))
+                    {
+                        context.Succeed(requirement);
+                    }
+                }
+            }
+            return Task.CompletedTask;
+
+        }
+    }
+}
---
+++ Chapter11/MinimalAPIServiceJS/CityBreaks/Pages/PropertyManager/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using CityBreaks.Models;
+using CityBreaks.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages.PropertyManager
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IPropertyService _propertyService;
+
+        public DeleteModel(IPropertyService propertyService)
+        {
+            _propertyService = propertyService;
+        }
+
+        public Property Property { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int Id { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Property = await _propertyService.FindAsync(Id);
+
+            if (Property == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _propertyService.DeleteAsync(Id);
+            return RedirectToPage("./Index");
+        }
+    }
+}
