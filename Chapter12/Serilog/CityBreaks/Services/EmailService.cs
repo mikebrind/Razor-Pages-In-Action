f4f90ef75fc26ca477b3f5b89e41e77e69104e27
Mike Brind <mikebrind@stepcomms.com>
2022-01-05 11:40:50 +0000

    Added Serilog

---
+++ Chapter12/Serilog/CityBreaks/Services/EmailService.cs
@@ -0,0 +1,72 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+using MimeKit.IO;
+
+namespace CityBreaks.Services
+{
+    public class EmailService : IEmailSender
+    {
+        private readonly IWebHostEnvironment _environment;
+        public EmailService(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var pickupDirectory = Path.Combine(_environment.ContentRootPath, "TempMail");
+            var message = new MimeMessage();
+            message.From.Add(MailboxAddress.Parse("test@test.com"));
+            message.To.Add(MailboxAddress.Parse(email));
+            message.Subject = subject;
+
+            message.Body = new TextPart("html")
+            {
+                Text = htmlMessage
+            };
+
+            await SaveToPickupDirectory(message, pickupDirectory);
+            await Task.CompletedTask;
+        }
+        public static async Task SaveToPickupDirectory(MimeMessage message, string pickupDirectory)
+        {
+            do
+            {
+                var path = Path.Combine(pickupDirectory, Guid.NewGuid().ToString() + ".eml");
+                Stream stream;
+
+                try
+                {
+                    stream = File.Open(path, FileMode.CreateNew);
+                }
+                catch (IOException)
+                {
+                    if (File.Exists(path))
+                        continue;
+                    throw;
+                }
+
+                try
+                {
+                    using (stream)
+                    {
+                        using var filtered = new FilteredStream(stream);
+                        filtered.Add(new SmtpDataFilter());
+
+                        var options = FormatOptions.Default.Clone();
+                        options.NewLineFormat = NewLineFormat.Dos;
+
+                        await message.WriteToAsync(options, filtered);
+                        await filtered.FlushAsync();
+                        return;
+                    }
+                }
+                catch
+                {
+                    File.Delete(path);
+                    throw;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ Chapter12/Serilog/CityBreaks/Services/ICityService.cs
@@ -0,0 +1,13 @@
+﻿using CityBreaks.Models;
+
+namespace CityBreaks.Services
+{
+    public interface ICityService
+    {
+        Task<List<City>> GetAllAsync();
+        Task<City> GetByNameAsync(string name);
+        Task<City> GetByIdAsync(int id);
+        Task<City> CreateAsync(City city);
+        Task<City> UpdateAsync(City city);
+    }
+}
