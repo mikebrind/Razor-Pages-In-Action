97ab83e8a5d6f5ec1b5c038678fdf33b2a92d00e
Mike Brind <mikebrind@live.co.uk>
2022-03-07 09:46:50 +0000

    Removed runtime compilation

--- Chapter02/WebApplication1/WebApplication1/Program.cs
+++ Chapter02/WebApplication1/WebApplication1/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
