e7f934a5cde7045128c0938142f88bc334b50f5e
Mike Brind <mikebrind@live.co.uk>
2022-03-09 08:33:25 +0000

    Removed previous examples

--- Chapter02/IMiddlewareExample/WebApplication1/Program.cs
+++ Chapter02/IMiddlewareExample/WebApplication1/Program.cs
@@ -8,12 +8,6 @@ builder.Services.AddTransient<IpAddressMiddleware>();
 
 var app = builder.Build();
 
-// register terminal middleware at the start of the pipeline to prevent any further processing
-
-// app.Run(TerminalMiddleware);
-// app.Run(async context => {
-//     await context.Response.WriteAsync("That�s all, folks!");
-// });
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -26,15 +20,6 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-// app.Use(PassThroughMiddleware);
-
-// typeof
-app.UseMiddleware(typeof(IpAddressMiddleware));
-// generic
-app.UseMiddleware<IpAddressMiddleware>();
-// extension method
-app.UseIpAddressMiddleware();
-
 app.UseRouting();
 
 app.UseAuthorization();
@@ -42,20 +27,3 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
-
-async Task TerminalMiddleware(HttpContext context)
-{
-    await context.Response.WriteAsync("That's all, folks!");
-}
-
-async Task PassThroughMiddleware(HttpContext context, Func<Task> next)
-{
-    if (context.Request.Query.ContainsKey("stop"))
-    {
-        await context.Response.WriteAsync("Stop the world");
-    }
-    else
-    {
-        await next();
-    }
-}
