3a1235cf0eddd76e66f975005d80ec7cba341ff3
Mike Brind <mikebrind@live.co.uk>
2022-03-11 07:29:32 +0000

    Remove unecessary braces

--- Chapter02/MiddlewareExample01/WebApplication1/Program.cs
+++ Chapter02/MiddlewareExample01/WebApplication1/Program.cs
@@ -7,9 +7,9 @@ var app = builder.Build();
 
 // register terminal middleware at the start of the pipeline to prevent any further processing
 // app.Run(TerminalMiddleware);
-// app.Run(async context => {
-//     await context.Response.WriteAsync("That�s all, folks!");
-// });
+// app.Run(async context => 
+//     await context.Response.WriteAsync("That's all, folks!");
+// );
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
